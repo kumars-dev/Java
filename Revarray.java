b9b25a77303dc38842d9e5d7931375e8404dc2cc
montukumar <montukumar98@gmail.com>
2022-11-28 13:30:38 +0530

    reverse of the array

---
+++ Revarray.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Revarray {
+    public static void main(String[] args) {
+        Scanner rev = new Scanner(System.in);
+        System.out.println("enter the size of the array");
+        int input = rev.nextInt();
+        int counter = input-1;
+        int element [] = new int[input];
+        System.out.println("enter the elements: ");
+        for(int i = 0;i<input;i++)
+        {
+            element[i]= rev.nextInt();
+        }
+        for(int i = 0;i<counter;i++)
+        {
+            int temp = element[i];
+            element[i] = element[counter];
+            element[counter] = temp;
+            counter--;
+        }
+
+        for(int i=0;i<input;i++)
+        {
+            System.out.println("element at " + i +" = "+ element[i]);
+        }
+    }
+}
