b97f2e75226540f3c8ffd3c066e97dccda58c922
montukumar <montukumar98@gmail.com>
2022-12-15 13:29:44 +0530

    2-d arrays

---
+++ twodarray.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+public class twodarray {
+    public static void main(String[] args) {
+        Scanner twod = new Scanner(System.in);
+        System.out.println("enter the rows");
+        int row = twod.nextInt();
+        System.out.println("enter the columns");
+        int column = twod.nextInt();
+        int[][] arr = new int[row][column];
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<column;j++)
+            {
+                arr[i][j]= twod.nextInt();
+            }
+            // System.out.println();
+        }
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<column;j++)
+            {
+                System.out.print(arr[i][j]+ " ");
+            }
+        System.out.println();
+        }
+        twod.close();
+    }
+}
